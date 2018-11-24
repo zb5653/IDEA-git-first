97a29db59b8c1a69486a352b165522f65db4e0c6
zb5653 <37863466+zb5653@users.noreply.github.com>
2018-11-25 00:54:51 +0800

    Update Test01.java

--- src/com/test/Test01.java
+++ src/com/test/Test01.java
@@ -2,6 +2,7 @@ package com.test;
 
 public class Test01 {
     Test test = new Test("","");
+    Test test1 = new Test("","");
     Test test2 = new Test("","");
     Test test3 = new Test("","");
 }
