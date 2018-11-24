6e086be7c9f2db78020fd6fea3d7424f73bf3ba4
zhangbin0706 <binzhang0706@163.com>
2018-11-25 00:26:05 +0800

    first commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,27 @@
+package com.test;
+
+public class Test {
+    public String name;
+    public String sex;
+
+    public Test(String name, String sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/com/test/Test01.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public class Test01 {
+    Test test = new Test("","");
+}
